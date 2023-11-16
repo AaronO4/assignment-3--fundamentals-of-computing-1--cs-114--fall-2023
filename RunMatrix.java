a517caed6fbb2eae71a2bbcfc05ff8ace14effd8
AaronO4 <143655787+AaronO4@users.noreply.github.com>
2023-11-15 22:47:46 -0500

    h

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-public int value = 1;
 public static int size;
 
   public static void main(String[] args) {
 
-  // final String RED = "\033[31m";
-  // final String RESET = "\033[0m";
   Scanner scan = new Scanner(System.in);
   System.out.println("Please enter the size of your matrix:");
   final int size = scan.nextInt();
   System.out.println("Your matrix is " + size + " x " + size);
   scan.close();
 
-    // Matrix matrix = new Matrix();
       Matrix.flipMatrix();
       Matrix.printMatrix();
       Matrix.populateMatrix();
