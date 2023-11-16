d6d21f43880641a3339c930fecdde9e75842c0fe
AaronO4 <143655787+AaronO4@users.noreply.github.com>
2023-11-15 20:12:14 -0500

    swap function added
    private static

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+
+  private static void swap(int row1, int col1, int row2, int col2){
+    int[][] table = new int[row1][col1];
+
+    int hold = table[row1][col1];
+    table[row1][col1] = table[row2][col2];
+    table[row2][col2] = hold;
+  }
+
   public static void main(String[] args) {
 
   int value = 1;
+  final String RED = "\033[31m";
+  final String RESET = "\033[0m";
   Scanner scan = new Scanner(System.in);
   System.out.println("Please enter the size of your matrix:");
   int size = scan.nextInt();
@@ -37,23 +48,16 @@ System.out.println("Printing matrix:");
 System.out.println("Flipping matrix... matrix flipped.");
 System.out.println("Printing flipped matrix:");
 
-// int x = table[row].length;
-// int y = table[col].length;
-// int temp;
-// temp = x;
-// x = y;
-// y = temp;
-
 // size-1 = table[row].length+table[col].length;
 //flipped version has to go here
 
-
-  for(int row = 0; row < table.length; row++){ // prints array
+  for(int row = 0; row < table.length / 2; row++){ // prints array
       for(int col = 0; col < table[row].length; col++) {
-          System.out.print(table[row][col] + "\t");
+          if(row + col != size - 1){
+            swap(row, col, size - row, size - col);
+          }
       }
-  System.out.println();
-  }
-
-  }
+        System.out.println();
+    }
+}
 }
