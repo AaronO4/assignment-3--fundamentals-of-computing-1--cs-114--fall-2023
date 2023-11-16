d0519839d97f4f0a8d5038179c6c9833e58709f4
AaronO4 <143655787+AaronO4@users.noreply.github.com>
2023-11-15 21:47:06 -0500

    public variables
    fixed redundancy

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,27 +1,19 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-
-  private static void swap(int x1, int y1, int x2, int y2){
-    int[][] table = new int[x1][y1];
-
-    int hold = table[x1][y1];
-    table[x1][y1] = table[x2][y2];
-    table[x2][y2] = hold;
-  }
+public static int[][] matrix;
+public static int size;
+public int value = 1;
 
   public static void main(String[] args) {
 
-  int value = 1;
   final String RED = "\033[31m";
   final String RESET = "\033[0m";
   Scanner scan = new Scanner(System.in);
   System.out.println("Please enter the size of your matrix:");
-  int size = scan.nextInt();
+  final int size = scan.nextInt();
   System.out.println("Your matrix is " + size + " x " + size);
   scan.close();
-
-int[][] matrix = new int[size][size];
   }
 
 public void printMatrix(){
@@ -36,6 +28,8 @@ System.out.println("Printing matrix with default values:");
 
 // loads values into the table
 public void populateMatrix(){
+  int value = 1;
+
   System.out.println("Populating matrix... matrix populated.");
   System.out.println("Printing matrix:");
 
@@ -44,13 +38,22 @@ public void populateMatrix(){
         matrix[row][col] = value++;
         System.out.print(matrix[row][col] + "\t");
       }
-
   System.out.println();
   }
 }
 
+private static void swap(int x1, int y1, int x2, int y2){
+  int[][] table = new int[x1][y1];
+
+  int hold = table[x1][y1];
+  table[x1][y1] = table[x2][y2];
+  table[x2][y2] = hold;
+}
+
 //flipped version has to go here
 public void flipMatrix(){
+    int[][] matrix = new int[size][size];
+
   System.out.println("Flipping matrix... matrix flipped.");
   System.out.println("Printing flipped matrix:");
 
